54d7de3914cdeeddabd9752f44b0dd8ad0baaf63
NatthaphongS <sinsupharoek@gmail.com>
2023-08-31 16:22:30 +0700

    finish todoitem

--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,9 +1,8 @@
-import { FaTrashAlt, FaPen } from 'react-icons/fa';
-import { HiOutlineCheck } from 'react-icons/hi';
 import { useState } from 'react';
 
 import styles from './TodoLists.module.scss';
-import TodoForm from './TodoForm';
+
+import TodoItem from './TodoItem';
 
 function TodoLists() {
   const [isOpenForm,setIsOpenForm]=useState(false)
@@ -13,23 +12,8 @@ function TodoLists() {
   }
   return (
     <ul className={styles.todo__lists}>
-      {isOpenForm ? <TodoForm textSubmit="Edit Task" setIsOpenForm={setIsOpenForm}/> :
-      (<li className={styles.todo}>
-        <div className={`${styles.todo__checkbox} ${styles.todo__checkbox__done}`}>
-          <HiOutlineCheck className={styles.todo__checkbox__icon} />
-        </div>
-        <p className={`${styles.todo__task} ${styles.todo__task__done}`}>todo-item 1 </p>
-        <span className={styles.todo__date}>30 Aug</span>
-        <div className={styles.todo__action}>
-          <span onClick={openEdit}>
-            <FaPen className={styles.todo__edit} />
-          </span>
-          <span>
-            <FaTrashAlt className={styles.todo__delete} />
-          </span>
-        </div>
-      </li>)
-      }
+      <TodoItem/>
+      <TodoItem/>
     </ul>
   );
 }
